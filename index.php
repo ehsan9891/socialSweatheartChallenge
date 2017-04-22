156f80f5b092731a9a637b347799300b4adce768
Ehsan Razmkhah <ehsan9891@gmail.com>
2017-04-22 10:08:11 +0200

    adding router to project

--- index.php
+++ index.php
@@ -1,11 +1,35 @@
-<html>
-    <head>
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require __DIR__.'/vendor/autoload.php';
 
-    </head>
-    <body>
-       <?php
-       
-       ?>
 
-    </body>
-</html>
+use Szenis\Router;
+use Szenis\RouteResolver;
+
+
+$router = new Router();
+
+$router->add('/fbapp/', 'GET', function() {
+    
+    echo "test";
+    return; 
+});
+
+
+$resolver = new RouteResolver($router);
+
+try {
+    // You have to resolve the route inside the try block
+    $resolver->resolve([
+        'uri' => $_SERVER['REQUEST_URI'],
+        'method' => $_SERVER['REQUEST_METHOD'],
+    ]);
+} catch (Szenis\Exceptions\RouteNotFoundException $e) {
+    // route not found, add a nice 404 page here if you like 
+    die($e->getMessage());
+} catch (Szenis\Exceptions\InvalidArgumentException $e) {
+    // when an arguments of a route is missing an InvalidArgumentException will be thrown 
+    // it is not necessary to catch this exception as this exception should never occur in production
+    die($e->getMessage());
+}
